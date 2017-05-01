4971099ffe6c75a3614f087b8ea00dab6ca766a5
Jonathan McCann <jonathan.mccann@liferay.com>
2017-05-01 13:02:35 -0700

    Utilize IFTTT for phone notifications

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+var config = require('./config.json');
 var dash_button = require('node-dash-button');
+var http = require('http');
 
-var dash_button_addresses = require('./dash_buttons.json');
+var laundry_dash = dash_button(config["laundry_mac_address"], null, null, 'all');
 
-var laundry_dash = dash_button(dash_button_addresses["laundry_mac_address"], null, null, 'all');
+var laundry_options = {
+	host: 'maker.ifttt.com',
+	path: '/trigger/laundry_done/with/key/' + config["ifttt_key"]
+}
+
+// 4500000 milliseconds equals 1 hour and 15 minutes
+var laundry_timeout = 4500000;
+
+function sendLaundryNotification() {
+	http.get(laundry_options, function(response) {
+	}).on("error", function(e) {
+		console.error(e);
+	})
+}
 
 laundry_dash.on("detected", function (){
-	console.log("Found laundry dash button");
+	setTimeout(sendLaundryNotification, laundry_timeout);
 });
